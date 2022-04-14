2f4d4746e04204a25a0e85c344d5ee7ebd41337e
chuma6619 <chuma6619@gmail.com>
2022-04-14 23:20:53 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Redirectpage from './Components/Redirectpage';
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter>
         <div className="App">
           <Routes>
-          <Route path="/" element={<Form />} />
-          <Route path="/Redirectpage" element={<Redirectpage />} />
+          <Route exact path="/" element={<Form />} />
+          <Route path="Redirectpage" element={<Redirectpage />} />
           </Routes>
           
       
